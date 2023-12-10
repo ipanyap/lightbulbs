af8a51ebdd0c946c6a9eeceec0c6b16a79d780df
Ivan Jayadi <ivan.j.jap@gmail.com>
2023-12-10 19:05:09 +0700

    1-set-up-typescript-monorepo: fix stylings

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const App = () => (
-    <div>
-        <h1>Hello World! I am React app</h1>
-    </div>
+  <div>
+    <h1>Hello World! I am React app</h1>
+  </div>
 );
 
 export default App;
